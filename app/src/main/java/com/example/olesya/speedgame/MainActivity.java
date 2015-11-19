2bcc8aabcc2dbe0af71a9ff6737c3d59980776ec
itamar sharify <itamarsharify@gmail.com>
2015-11-19 11:12:17 +0200

    refactored and cleand code

--- app/src/main/java/com/example/olesya/speedgame/MainActivity.java
+++ app/src/main/java/com/example/olesya/speedgame/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.olesya.speedgame;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Button firstButton, secondButton;
     private TextView timTextView, beTextView;
     private long startTime = 0, endTime = 0, bestTime = 0, gameTime = 0;
-    private String btime, gtime;
+    private boolean gameResetted = false;
+    private String btime, gtime, isNew;
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor e;
     private String left, right;
@@ -41,22 +47,30 @@ public class MainActivity extends AppCompatActivity {
         btime = mSharedPreferences.getString("BestScore", "");
         left = mSharedPreferences.getString("Left","");
         right = mSharedPreferences.getString("Right","");
-
+        isNew = mSharedPreferences.getString("isNew","false");
         //first initializing
         if(left.isEmpty() && right.isEmpty()) {
             setNumbers();//set the random numbers to the buttons
         }
-        //load scores from memory
-
 
         //If there is a data show it, if not leave empty
-        if(!gtime.isEmpty()) {
-            timTextView.setText(gtime+" ms");
-            gameTime = Long.parseLong(gtime);
+        gameResetted = Boolean.parseBoolean(isNew);
+        if(!gameResetted)
+        {
+
+            if(!gtime.isEmpty()) {
+                timTextView.setText(gtime+" ms");
+                gameTime = Long.parseLong(gtime);
+            }
+            if(!btime.isEmpty()) {
+                beTextView.setText(btime+" ms");
+                bestTime = Long.parseLong(btime);
+            }
         }
-        if(!btime.isEmpty()) {
-            beTextView.setText(btime+" ms");
-            bestTime = Long.parseLong(btime);
+        else{
+            timTextView.setText("");
+            beTextView.setText("");
+
         }
 
         /* Define listener to first button to start time counter */
@@ -79,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
         beTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                gameResetted = true;
                 bestTime = 0;
                 gameTime = 0;
                 if(beTextView.getText().length()>0)
@@ -89,50 +104,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
     @Override
     protected void onStop() {
         super.onStop();
         //save data to memry before the app stops
         mSharedPreferences = getSharedPreferences("myfile", MODE_PRIVATE);
         e = mSharedPreferences.edit();
-        if(bestTime>0)
+        if(!gameResetted)
+        {
             e.putString("BestScore", ""+ bestTime);
-        else
-            e.putString("BestScore", "");
-        if(gameTime>0)
             e.putString("CurrentScore", ""+ gameTime);
-        else
+        }
+        else{
+            e.putString("BestScore", "");
             e.putString("CurrentScore", "");
-//        e.putString("CurrentScore", ""+ gameTime);
+        }
         e.putString("Left", ""+ left);
         e.putString("Right", ""+ right);
+        e.putString("isNew", ""+gameResetted);
         e.apply();
 
 
     }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     //start counting the time
     //calculation done by calculating the ms time difference
@@ -167,6 +160,7 @@ public class MainActivity extends AppCompatActivity {
     //sets both buttons clicks
     private void buttonFunction(String s)
     {
+        gameResetted = false;
        switch (s)//check which bummton are you now
        {
            case "1":
@@ -181,6 +175,19 @@ public class MainActivity extends AppCompatActivity {
                    if (bestTime == 0 | bestTime > gameTime){
                        bestTime = gameTime;
                        Toast.makeText(this,R.string.newHighScore,Toast.LENGTH_SHORT).show();
+                       new CountDownTimer(3000,500){
+                           public void onTick(long millisUntilFinished) {
+                               if((millisUntilFinished/500)%2==0)
+                                beTextView.setBackgroundColor(Color.CYAN);
+                               else
+                                   beTextView.setBackgroundColor(Color.BLUE);
+
+                           }
+
+                           public void onFinish() {
+                               beTextView.setBackgroundColor(Color.BLUE);
+                           }
+                       }.start();
                    }
                    beTextView.setText(bestTime + " ms");
                    setNumbers();//set new random numbers
